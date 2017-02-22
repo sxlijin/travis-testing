e474f59c09531befff93dea4b0fb3fdb9bbfb49a
Samuel Lijin <sxlijin@gmail.com>
2017-02-22 07:18:31 -0600

    add sample C, C++ programs

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("printing from main.c\n");
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "printing from main.cpp" << std::endl;
+    return 0;
+}
